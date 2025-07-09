09b784641ce8e0fa69c42d8945b18e8ac2134a9c
shavkatJuraboyev <shavkatjoraboyev0@gmail.com>
2025-07-09 14:39:59 +0500

    'yangilandi'

--- samtuit_uz/settings.py
+++ samtuit_uz/settings.py
@@ -108,23 +108,23 @@ WSGI_APPLICATION = 'samtuit_uz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-   }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'samtuit',
-#         'USER': 'samtuituser',
-#         'PASSWORD': 'samtuit2025',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'samtuit',
+        'USER': 'samtuituser',
+        'PASSWORD': 'samtuit2025',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
